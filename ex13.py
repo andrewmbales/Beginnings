33411fa6fe931cbb654aac82d81c5ebc260319c4
andrewmbales <andrewmbales@gmail.com>
2017-04-11 22:08:42 -0600

    Continuing Learning
    Continuing to code in Python

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is: ", third
+
+print "Say something below!"
+raw_input()
+print "I'm not sure how to reply... yet. ;)"
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Andrew M. Bales'
+my_age = 21
+my_height = 69 #inches
+my_weight = 175 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White' #thankfully!!
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's pretty heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s, depending on how many cups of Joe he's had that day." % my_teeth
+
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
